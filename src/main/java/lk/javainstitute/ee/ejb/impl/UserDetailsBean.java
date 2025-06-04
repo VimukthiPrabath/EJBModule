2c50f34b640b621569033c2b719b62d732cee2d6
VimukthiPrabhath <vimukthivpk123@gmail.com>
2025-06-04 20:32:55 +0530

    3rd day

--- src/main/java/lk/javainstitute/ee/ejb/impl/UserDetailsBean.java
+++ src/main/java/lk/javainstitute/ee/ejb/impl/UserDetailsBean.java
@@ -2,24 +2,26 @@ package lk.javainstitute.ee.ejb.impl;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import jakarta.ejb.EJB;
-import jakarta.ejb.Stateless;
+import jakarta.ejb.*;
 import lk.javainstitute.ee.ejb.remote.AppSettings;
 import lk.javainstitute.ee.ejb.remote.UserDetails;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-@Stateless(name = "UserInfo", mappedName = "XYZ")
+@Singleton
+@Startup
 public class UserDetailsBean implements UserDetails {
 
 
     //dependency injection
     @EJB private AppSettings settings;
 
+    private int counter;
+
     @PostConstruct
     public void init() {
-        System.out.println("UserDetailsBean init");
+        System.out.println("UserDetailsBean init"+this);
     }
 
     @PreDestroy
@@ -28,9 +30,11 @@ public class UserDetailsBean implements UserDetails {
     }
 
     @Override
+    @Lock(LockType.WRITE)
     public String getName() {
 
-     return settings.getAppName() +" "+settings.getAppVersion()+" "+settings.getAppDescription();
+        counter++;
+     return settings.getAppName() +" "+counter;
 
    }
     @Override
